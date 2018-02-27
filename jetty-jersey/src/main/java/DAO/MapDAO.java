579d5327dd2f489f74916e99be6e3ea6f63febad
querolj <jean.querol@univ-paris-diderot.fr>
2018-02-27 15:17:04 +0100

    projet eclipse DAO (bouchon null

---
+++ jetty-jersey/src/main/java/DAO/MapDAO.java
@@ -0,0 +1,48 @@
+package DAO;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class MapDAO implements MapInterface{
+
+	public ArrayList<Map> getMaps() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<Map> getMaps(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<Map> getMaps(LocalDateTime date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<Map> getPublicMaps() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<Map> getPublicMaps(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<Map> getFriendsMap(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void createMap() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deleteMap(Map map) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
