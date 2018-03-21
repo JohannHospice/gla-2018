d68a4d8812919544bb80cb14c4d0b761642427a6
querolj <jean.querol@univ-paris-diderot.fr>
2018-03-21 10:27:59 +0100

    getAllUsers dispo

--- src/main/java/com/example/jetty_jersey/ElasticTest.java
+++ src/main/java/com/example/jetty_jersey/ElasticTest.java
@@ -19,6 +19,8 @@ import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.*;
@@ -26,7 +28,15 @@ import org.elasticsearch.client.transport.*;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.rest.RestStatus;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 import DAO.Location;
 import DAO.User;
@@ -59,17 +69,7 @@ public class ElasticTest {
 		//OpenIndexResponse openIndexResponse = client.indices().open(request);
 		//boolean acknowledged = openIndexResponse.isAcknowledged();
 		//System.out.println(acknowledged);
-		
-		// INDEX REQUEST_______________________________________
-		/*
-		Map<String, Object> jsonMap = new HashMap<String, Object>();
-		jsonMap.put("user", "kimchy");
-		jsonMap.put("postDate", new Date(0));
-		jsonMap.put("message", "trying out Elasticsearch");
-		IndexRequest indexRequest = new IndexRequest("posts", "doc", "1")
-		        .source(jsonMap);
-		
-		IndexResponse indexResponse = client.index(indexRequest);*/
+
 		
 		// UPDATE_______________________________________
 		
@@ -98,17 +98,57 @@ public class ElasticTest {
 		user2.mapList = new ArrayList<String>();
 		user3.mapList = new ArrayList<String>();
 		user2.friendList =  new ArrayList<String>();
+		user2.friendList.add(user3.username);
 		user3.friendList = f2;                                                            
 		
 		InsertUser(user, client);
+		//User user_get = GetUser("jean", client);
+		//System.out.println("get : "+user);
+		GetAllUsers(client);
+		client.close();
+	
 		
-		// GET _________________________________________
+	}
+	
+	
+	
+	
+	public static void InsertUser(User user, RestHighLevelClient client) throws JsonProcessingException
+	{
 		
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		String json = ow.writeValueAsString(user);
+		
+		Map<String, Object> jsonMap = new HashMap<String, Object>();
+		/*
+		jsonMap.put("username", user.username);
+		jsonMap.put("password", user.password);
+		jsonMap.put("mail", user.mail);
+		jsonMap.put("friends", user.friendList);
+		jsonMap.put("maps", user.mapList);
+		*/
+		jsonMap.put("username", json);
+		
+		
+		IndexRequest indexRequest = new IndexRequest("posts", "doc",user.username)
+		        .source(jsonMap);
+		
+		try {
+			IndexResponse indexResponse = client.index(indexRequest);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static User GetUser(String username, RestHighLevelClient client) throws IOException
+	{
+		User user = null;
 		GetRequest getRequest = new GetRequest(
 		        "posts", 
 		        "doc",  
-		        "2"); 
-		
+		        username); 
+		String sourceAsString = "";
 		try {
 			
 			GetResponse getResponse = client.get(getRequest);
@@ -117,7 +157,7 @@ public class ElasticTest {
 			String id = getResponse.getId();
 			if (getResponse.isExists()) {
 			    long version = getResponse.getVersion();
-			    String sourceAsString = getResponse.getSourceAsString();        
+			    sourceAsString = getResponse.getSourceAsString();        
 			    Map<String, Object> sourceAsMap = getResponse.getSourceAsMap(); 
 			    byte[] sourceAsBytes = getResponse.getSourceAsBytes();    
 			    System.out.println(sourceAsString+"\n"+sourceAsMap.toString());
@@ -129,38 +169,46 @@ public class ElasticTest {
 		        
 		    }
 		}
-		
-		client.close();
-	
-		
+
+		user = new ObjectMapper().readValue(sourceAsString, User.class);
+
+
+		return user;
 	}
 	
-	
-	
-	
-	public static void InsertUser(User user, RestHighLevelClient client)
+	public static ArrayList<User> GetAllUsers(RestHighLevelClient client) throws IOException
 	{
-		Map<String, Object> jsonMap = new HashMap<String, Object>();
-		jsonMap.put("username", user.username);
-		jsonMap.put("password", user.password);
-		jsonMap.put("mail", user.mail);
-		jsonMap.put("friends", user.friendList);
-		jsonMap.put("maps", user.mapList);
+		ArrayList<User> users = new ArrayList<User>();
 		
+		SearchRequest searchRequest = new SearchRequest(); 
+		SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder(); 
+		searchSourceBuilder.query(QueryBuilders.matchAllQuery()); 
+		searchRequest.source(searchSourceBuilder);
 		
-		IndexRequest indexRequest = new IndexRequest("posts", "doc")
-		        .source(jsonMap);
+		SearchResponse searchResponse = client.search(searchRequest);
+		SearchHits hits = searchResponse.getHits();
+		long totalHits = hits.getTotalHits();
+		float maxScore = hits.getMaxScore();
+		System.out.println("total : "+totalHits+", maxScore : "+maxScore);
 		
-		try {
-			IndexResponse indexResponse = client.index(indexRequest);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		SearchHit[] searchHits = hits.getHits();
+		for (SearchHit hit : searchHits) {
+		    // do something with the SearchHit
+			String index = hit.getId();
+			
+			String sourceAsString = hit.getSourceAsString();
+			User user = new ObjectMapper().readValue(sourceAsString, User.class);
+			
+			users.add(user);
 		}
+		
+		return users;
 	}
+	
 	
 	public static void InsertMap(DAO.Map map, RestHighLevelClient client)
 	{
+		
 		Map<String, Object> jsonMap = new HashMap<String, Object>();
 		//jsonMap.put("username", user.username);
 		
