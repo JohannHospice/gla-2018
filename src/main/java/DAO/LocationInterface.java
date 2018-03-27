b80879d1f8f81e883fc92921dafb9d680b771966
querolj <jean.querol@univ-paris-diderot.fr>
2018-03-27 13:21:30 +0200

    ajout dune fonction connection

--- src/main/java/DAO/LocationInterface.java
+++ src/main/java/DAO/LocationInterface.java
@@ -12,34 +12,8 @@ public interface LocationInterface {
      * @return the list of all locations 
      */
     ArrayList<Location> getLocations(RestHighLevelClient client, String map_name) throws IOException;
-    
-    /** 
-     * @param date
-     * @return the list of locations created after the date
-     */
-    ArrayList<Location> getDatedLocations(RestHighLevelClient client, Date date) throws IOException;
-    
-    /** 
-     * @param date
-     * @param map
-     * @return the list of locations created after the date on a specific map
-     */
-    ArrayList<Location> getDatedLocations(RestHighLevelClient client, Date date, Map map) throws IOException;
-    
-    
-    /**
-     * 
-     * @return the list of all favorite locations
-     */
-    ArrayList<Location> getFavoriteLocations(RestHighLevelClient client) throws IOException;
-    
-    /**
-     * 
-     * @param map
-     * @return the list of favorite locations on the map
-     */
-    ArrayList<Location> getFavoriteLocations(RestHighLevelClient client, Location location, Map map) throws IOException;
-    
+       
+     
     /**
      * 
      * @param map, client
