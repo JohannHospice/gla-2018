7a35a0e227e76750e0a406648a11ba458e3cd4ac
Elouan Martinet <exa@elou.world>
2018-04-27 12:34:49 +0000

    return false when signup failed

--- src/main/java/com/example/jetty_jersey/ws/auth/AuthResource.java
+++ src/main/java/com/example/jetty_jersey/ws/auth/AuthResource.java
@@ -62,19 +62,21 @@ public class AuthResource extends Ressource{
                 user.setMail(email);
                 user.setPassword(password);
                 user.setUsername(username);
-                
-                Map map = new Map(username,"random");
-                user.addMap(map.getName());
-                
-                Map map2 = new Map(username,"random2");
-                user.addMap(map.getName());
 
-                DAO.getActionMap().insertMap(DAO.client, map);
-                DAO.getActionMap().insertMap(DAO.client, map2);
-				
-                DAO.getActionUser().insertUser(DAO.client, user);
-                
-                return new SimpleResponse(true);
+                if(DAO.getActionUser().insertUser(DAO.client, user))
+				{
+					Map map = new Map(username,"random");
+					user.addMap(map.getName());
+					Map map2 = new Map(username,"random2");
+					user.addMap(map.getName());
+
+					DAO.getActionMap().insertMap(DAO.client, map);
+					DAO.getActionMap().insertMap(DAO.client, map2);
+
+                    return new SimpleResponse(true);
+				}
+				else
+                    return new SimpleResponse(false);
             }
         } catch (IOException e) {
             e.printStackTrace();
